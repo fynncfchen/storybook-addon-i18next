ca74b04a02b12d89f5d8e9d55d85d4f0bba0e030
Ethan Chen <ethan.chen@gogoro.com>
2019-07-09 10:13:24 +0800

    Add build for ES module

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        shippedProposals: true,
+        useBuiltIns: 'usage',
+        corejs: '3',
+      },
+    ],
     '@babel/preset-react',
   ],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
+  plugins: ['@babel/plugin-proposal-object-rest-spread'],
 };
