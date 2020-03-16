f90420a61c66ade8a61b236aa0b4b1f73c319d67
fynnchen <fynn.chen@samba.tv>
2020-03-16 10:59:15 +0800

    Update package settings

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  parser: 'babel-eslint',
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error'],
-  },
-};
